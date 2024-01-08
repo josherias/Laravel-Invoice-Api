355f9c388e5721bc655f03e89d21aa667c05cd2b
Josh Erias <josherias10@gmail.com>
2024-01-08 18:38:40 +0300

    Filtering complete

--- app/Services/V1/CustomerQuery.php
+++ app/Services/V1/CustomerQuery.php
@@ -5,6 +5,7 @@ namespace App\Services\V1;
 use Illuminate\Http\Request;
 
 class CustomerQuery {
+    
  protected $safeParams = [
     'name' => ['eq'],
     'type' => ['eq'],
@@ -16,6 +17,10 @@ class CustomerQuery {
  ];   
 
 
+ protected $columnMap = [
+    'postalCode' => 'postal_code'
+ ];
+
  protected $operatorMap = [
     'eq' => '=',
     'lt' => '<',
@@ -23,4 +28,29 @@ class CustomerQuery {
     'gt' => '>',
     'gte' => '>='
  ];
+
+
+ public function transform(Request $request){
+    $eloQuery = [];
+
+    foreach($this->safeParams as $param => $operators){
+
+        $query = $request->query($param);
+
+        if(!isset($query)){
+            continue;
+        }
+
+        $column = $this->columnMap[$param] ?? $param;
+
+        foreach ($operators as $operator){
+            if(isset($query[$operator])){
+                $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+            }
+        }
+
+    }
+
+    return $eloQuery;
+ }
 }
